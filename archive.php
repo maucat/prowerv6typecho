0a7a490b33010c10179428ac114ac7ed98889be8
Maucat <zjc009@gmail.com>
2013-11-30 16:13:18 +0800

    1.修改pre code blockquote hr样式 2.每个文章后加入2px分隔线

--- archive.php
+++ archive.php
@@ -9,16 +9,25 @@
         ), '', ''); ?></div>
         <?php if ($this->have()): ?>
             <article id="post_list">
-            <?php while($this->next()): ?>
+            <?php while($this->next()): $format = PostFormat_Plugin::getFormat(); ?>
         	<section class="post">
-
-        	<span class="icon icon_aside" title="日志"></span>
-
+                    <?php if ($format == 'video') : ?>
+                        <span class="icon icon_video" title="视频"></span>
+                    <?php elseif ($format == 'audio') : ?>
+                        <span class="icon icon_audio" title="音乐"></span>
+                    <?php elseif ($format == 'gallery') : ?>
+                        <span class="icon icon_gallery" title="相册"></span>
+                    <?php elseif ($format == 'aside') : ?>
+                        <span class="icon icon_aside" title="日志"></span>
+                    <?php else : ?>
+                        <span class="icon icon_aside" title="文章"></span>
+                    <?php endif; ?>
         	<h2 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
         	<time class="time" datetime="<?php $this->date('Y-m-d'); ?>"><?php $this->date('Y.m.d'); ?></time>
                     <div class="post-content">
             			<?php $this->content('阅读详细 &raquo;'); ?>
                     </div>
+                <hr class="soften" />
                 </section>
             <?php endwhile; ?>
             </article>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * 根据Prower V6移植的主题
  * 
  * @package Prower V6 for Typecho
- * @author maucat
+ * @author alter by maucat
  * @version 1.2
  * @link http://soont.com
  */
@@ -16,28 +16,23 @@
     <article id="post_list">
     <?php while($this->next()): $format = PostFormat_Plugin::getFormat(); ?>
 	<section class="post">
-<?php if ($format == 'video') : ?>
-<span class="icon icon_video" title="视频"></span>
-<?php elseif ($format == 'audio') : ?>
-<span class="icon icon_audio" title="音乐"></span>
-<?php elseif ($format == 'gallery') : ?>
-<span class="icon icon_gallery" title="相册"></span>
-<?php elseif ($format == 'aside') : ?>
-<span class="icon icon_aside" title="日志"></span>
-<?php else : ?>
-<span class="icon icon_aside" title="文章"></span>
-<?php endif; ?>
-
+            <?php if ($format == 'video') : ?>
+                <span class="icon icon_video" title="视频"></span>
+            <?php elseif ($format == 'audio') : ?>
+                <span class="icon icon_audio" title="音乐"></span>
+            <?php elseif ($format == 'gallery') : ?>
+                <span class="icon icon_gallery" title="相册"></span>
+            <?php elseif ($format == 'aside') : ?>
+                <span class="icon icon_aside" title="日志"></span>
+            <?php else : ?>
+                <span class="icon icon_aside" title="文章"></span>
+            <?php endif; ?>
 	<h2 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
 	<time class="time" datetime="<?php $this->date('Y-m-d'); ?>"><?php $this->date('Y.m.d'); ?></time>
-            <p>format: <?php echo $format ?></p>
-            <?php if ($format == 'video') : ?>
-            <p>这是video</p>
-            <?php endif; ?>
-
             <div class="post-content">
     			<?php $this->content('阅读详细 &raquo;'); ?>
             </div>
+        <hr class="soften" />
         </section>
     <?php endwhile; ?>
     </article>
