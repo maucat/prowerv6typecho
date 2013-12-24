8985965b7589254c318c5c86af58933ccab86852
Maucat <zjc009@gmail.com>
2013-12-25 00:50:10 +0800

    修改post页面的文章形式

--- post.php
+++ post.php
@@ -2,6 +2,26 @@
 <div id="main">
     <div class= "post">
     <span class="icon icon_aside" title="文章"></span>
+    <?php $format = PostFormat_Plugin::getFormat(); ?>
+            <?php if ($format == 'video') : ?>
+                <span class="icon icon_video" title="视频"></span>
+            <?php elseif ($format == 'audio') : ?>
+                <span class="icon icon_audio" title="音乐"></span>
+            <?php elseif ($format == 'gallery') : ?>
+                <span class="icon icon_gallery" title="相册"></span>
+            <?php elseif ($format == 'aside') : ?>
+                <span class="ico-aside" title="日志"></span>
+            <?php elseif ($format == 'link') : ?>
+                <span class="ico-link" title="链接"></span>
+            <?php elseif ($format == 'quote') : ?>
+                <span class="ico-quote" title="引语"></span>
+            <?php elseif ($format == 'status') : ?>
+                <span class="ico-status" title="心情"></span>
+            <?php elseif ($format == 'chat') : ?>
+                <span class="ico-chat" title="聊天"></span>
+            <?php else : ?>
+                <span class="icon icon_aside" title="文章"></span>
+            <?php endif; ?>
         <header id="post_header">
                 <h1><?php $this->title() ?></h1>
                 <time class="time" datetime="<?php $this->date('Y-m-d'); ?>"><?php $this->date('Y.m.d H:i'); ?></time>
